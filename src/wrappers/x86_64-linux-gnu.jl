6d3a5ebe4178e15474ed42457484ad4ea5f9b5b6
jlbuild <juliabuildbot@gmail.com>
2020-11-18 12:25:39 +0000

    NetCDF_jll build 4.7.4+1

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -4,58 +4,18 @@ export libnetcdf
 using HDF5_jll
 using Zlib_jll
 using LibCURL_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `libnetcdf`
-const libnetcdf_splitpath = ["lib", "libnetcdf.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libnetcdf_path = ""
-
-# libnetcdf-specific global declaration
-# This will be filled out by __init__()
-libnetcdf_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libnetcdf = "libnetcdf.so.18"
-
-
-"""
-Open all libraries
-"""
+using LibSSH2_jll
+using MbedTLS_jll
+using nghttp2_jll
+JLLWrappers.@generate_wrapper_header("NetCDF")
+JLLWrappers.@declare_library_product(libnetcdf, "libnetcdf.so.18")
 function __init__()
-    global artifact_dir = abspath(artifact"NetCDF")
+    JLLWrappers.@generate_init_header(HDF5_jll, Zlib_jll, LibCURL_jll, LibSSH2_jll, MbedTLS_jll, nghttp2_jll)
+    JLLWrappers.@init_library_product(
+        libnetcdf,
+        "lib/libnetcdf.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (HDF5_jll.PATH_list, Zlib_jll.PATH_list, LibCURL_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (HDF5_jll.LIBPATH_list, Zlib_jll.LIBPATH_list, LibCURL_jll.LIBPATH_list,))
-
-    global libnetcdf_path = normpath(joinpath(artifact_dir, libnetcdf_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libnetcdf_handle = dlopen(libnetcdf_path)
-    push!(LIBPATH_list, dirname(libnetcdf_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(LIBPATH_list, ':')
-
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
